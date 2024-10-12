21cfd3c1a1ec6c1f3bb35727ca11ed3dcb51e608
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2024-10-12 22:59:10 +0200

    Add logo and navbar

--- app/page.js
+++ app/page.js
@@ -1,17 +1,9 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <main>
       <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
       </h1>
-
-      <Link href="/meals">Meals</Link>
-      <br />
-      <Link href="/meals/share">Share Meals</Link>
-      <br />
-      <Link href="/community">Community</Link>
     </main>
   );
 }
